764654f2d3460554300b2e1d49d1c88fb28925be
Tamanh <tamvn@uw.edu>
2014-11-25 11:05:30 -0800

    Added API and REST keys. Basis for form function in HTML

--- js/app.js
+++ js/app.js
@@ -3,3 +3,15 @@
     app.js, main Angular application script
     define your module and controllers here
 */
+
+var baseUrl = 'https://api.parse.com/1/classes/';
+
+angular.module('review', [])
+    .config(function($httpProvider) {
+        $httpProvider.defaults.headers.common['X-Parse-Application-Id'] = 'vEKP8fXUoI63EczduB4VfwfnQmhwKXkScbQsGiDy';
+        $httpProvider.defaults.headers.common['X-Parse-REST-API-Key'] = 'JomRQU7xqTlREKDY7f2rcssCzeHt7LQX1KEyMf2l';
+    })
+
+    .controller('reviewController', function($scope, $http) {
+    
+}); // end controller
